e0a31d84dcdb3a1815e7105d2a6b071b7cb8bc64
Hui (QRD) Yu <huiyQuic@codeaurora.org>
2014-01-31 22:08:54 +0800

    Add 2 APIs (suspend/resume) in MediaPlayer
    - API:suspend() will just pause the player and release all the decoders
      to replace release() which will release the whole player
    - API:resume() will just init the decoders again,
      then start() will be called to start to play the streaming
    - Add a check in AwesomePlayer::onVideoEvent()
      to make sure the first seek operation will always seek the next i-frame
    
    Change-Id: Ie4c82906a2a056378119921a656128ebdc1007c4
    
    Conflicts:
    	media/libstagefright/AwesomePlayer.cpp
    	media/libstagefright/NuCachedSource2.cpp
    	media/libstagefright/include/NuCachedSource2.h

--- media/libstagefright/NuCachedSource2.cpp
+++ media/libstagefright/NuCachedSource2.cpp
@@ -194,7 +194,9 @@ NuCachedSource2::NuCachedSource2(
       mHighwaterThresholdBytes(kDefaultHighWaterThreshold),
       mLowwaterThresholdBytes(kDefaultLowWaterThreshold),
       mKeepAliveIntervalUs(kDefaultKeepAliveIntervalUs),
-      mDisconnectAtHighwatermark(disconnectAtHighwatermark) {
+      mDisconnectAtHighwatermark(disconnectAtHighwatermark),
+      mIsNonBlockingMode(false),
+      mSuspended(false) {
     // We are NOT going to support disconnect-at-highwatermark indefinitely
     // and we are not guaranteeing support for client-specified cache
     // parameters. Both of these are temporary measures to solve a specific
@@ -292,7 +294,7 @@ void NuCachedSource2::fetchInternal() {
         }
     }
 
-    if (reconnect) {
+    if (reconnect && !mSuspended) {
         status_t err =
             mSource->reconnectAtOffset(mCacheOffset + mCache->totalSize());
 
@@ -396,6 +398,12 @@ void NuCachedSource2::onFetch() {
         }
     } else {
         delayUs = 100000ll;
+    }
+
+    if (mSuspended) {
+        static_cast<HTTPBase *>(mSource.get())->disconnect();
+        mFinalStatus = -EAGAIN;
+        return;
     }
 
     (new AMessage(kWhatFetchMore, mReflector->id()))->post(delayUs);
@@ -706,6 +714,27 @@ void NuCachedSource2::RemoveCacheSpecificHeaders(
 
         ALOGV("Client requested disconnection at highwater mark");
     }
+}
+
+status_t NuCachedSource2::disconnectWhileSuspend() {
+    if (mSource != NULL) {
+        static_cast<HTTPBase *>(mSource.get())->disconnect();
+        mFinalStatus = -EAGAIN;
+        mSuspended = true;
+    } else {
+        return ERROR_UNSUPPORTED;
+    }
+
+    return OK;
+}
+
+status_t NuCachedSource2::connectWhileResume() {
+    mSuspended = false;
+
+    // Begin to connect again and fetch more data
+    (new AMessage(kWhatFetchMore, mReflector->id()))->post();
+
+    return OK;
 }
 
 }  // namespace android
